bead2fa25637f0352690feb18783ce70e7c80c53
seven <v-lpeiyang.gd@chinatelecom.cn>
2023-05-06 12:20:32 +0800

    finish date format action

--- common/utils/date/format.js
+++ common/utils/date/format.js
@@ -7,10 +7,16 @@ export default defineComponent({
   description: "Date Format",
   type: 'action',
   props: {
+    date: {
+      type: 'string',
+      label: 'Date',
+      description: 'Date, default to `new Date()`',
+      optional: true,
+    },
     timezone: {
       type: 'string',
       label: 'Timezone',
-      description: 'Timezone, eg: Asia/Shanghai, see [Timezone List](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)',
+      description: 'Timezone, eg: Asia/Shanghai, see [Timezone List](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones). default to UTC',
       optional: true,
       options: [
         { label: 'UTC', value: 'UTC' },
@@ -20,7 +26,7 @@ export default defineComponent({
     format: {
       type: 'string',
       label: 'Format',
-      description: 'Format String, eg: YYYY-MM-DDTHH:mm:ss.SSS[Z]',
+      description: 'Format String, eg: YYYY-MM-DDTHH:mm:ss.SSS[Z]. Format AS ISO8601 if not set',
       optional: true,
       options: [
         { label: 'YYYY-MM-DDTHH:mm:ss.SSS[Z]', value: 'YYYY-MM-DDTHH:mm:ss.SSS[Z]' },
@@ -29,16 +35,16 @@ export default defineComponent({
     }
   },
   async run({ steps, $ }) {
+    const date = Boolean(this.date) ? this.date : new Date();
     const timezone = this.timezone || 'UTC';
-    const format = this.format || 'YYYY-MM-DDTHH:mm:ss.SSS[Z]';
     // Return data to use it in future steps
     return {
       timezone: timezone,
-      format_date: moment.tz(new Date(), timezone).format(format),
+      format_date: moment.tz(date, timezone).format(this.format),
       iso8601: {
-        date: moment.tz(new Date(), timezone).format('YYYY-MM-DD'),
-        time: moment.tz(new Date(), timezone).format('HH:mm:ss.SSS[Z]'),
-        timestamp: moment.tz(new Date(), timezone).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
+        date: moment.tz(date, timezone).format('YYYY-MM-DD'),
+        time: moment.tz(date, timezone).format('HH:mm:ss.SSS[Z]'),
+        timestamp: moment.tz(date, timezone).format(),
       }
     };
   },
