7d0c544d7cea73cc2e2a149ea2ef5579176f4187
seven <v-lpeiyang.gd@chinatelecom.cn>
2023-06-28 17:07:41 +0800

    add common/utils/chinese

---
+++ common/utils/chinese/zhlint.mjs
@@ -0,0 +1,31 @@
+import { run } from 'zhlint';
+export default defineComponent({
+    name: 'zhlint',
+    version: '0.0.1',
+    key: 'zhlint',
+    description: "A linting tool for Chinese text content， see the [doc](https://github.com/Jinjiang/zhlint)",
+    type: 'action',
+    props: {
+        text: {
+            type: 'string',
+            label: 'Text',
+            description: 'plain text which need to be formate',
+        },
+    },
+
+    methods: {
+        zhlint(text) {
+            if (!Boolean(text)) {
+                return '';
+            }
+
+            const options = { rules: { preset: 'default' } };
+            const output = run(text, options);
+            console.log(output);
+            return output.result;
+        },
+    },
+    async run({ steps, $ }) {
+        return this.zhlint(this.text);
+    },
+})
